4284ed2bf70e34c87722579695ef29d547940da5
Matheus Gonçalves <matheuscg@ymail.com>
2018-10-14 21:52:11 -0300

    created user and statistic table, statistics are created analysing data

--- dynamodb.py
+++ dynamodb.py
@@ -1,7 +1,7 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
-tweet_table_schema = {
+tweets_table_schema = {
     'TableName':'tweets',
     'KeySchema': [
         {
@@ -9,7 +9,7 @@ tweet_table_schema = {
             'KeyType':'HASH'
         },
         {
-            'AttributeName':'category',
+            'AttributeName':'hashtag',
             'KeyType':'RANGE'
         }
     ],
@@ -19,7 +19,7 @@ tweet_table_schema = {
             'AttributeType': 'N'
         },
         {
-            'AttributeName': 'category',
+            'AttributeName': 'hashtag',
             'AttributeType': 'S'
         },
     ],
@@ -29,31 +29,119 @@ tweet_table_schema = {
     }
 }
 
+users_table_schema = {
+    'TableName':'users',
+    'KeySchema': [
+        {
+            'AttributeName':'id',
+            'KeyType':'HASH'
+        }
+    ],
+    'AttributeDefinitions':[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'N'
+        }
+    ],
+    'ProvisionedThroughput':{
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+}
+
+statistics_table_schema = {
+    'TableName':'statistics',
+    'KeySchema': [
+        {
+            'AttributeName':'type',
+            'KeyType':'HASH'
+        }
+    ],
+    'AttributeDefinitions':[
+        {
+            'AttributeName': 'type',
+            'AttributeType': 'N'
+        }
+    ],
+    'ProvisionedThroughput':{
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+}
+
+class statistics_type:
+    FIVE_MOST =  1
+    HOUR_DAY_GROUP = 2
+    HASHTAG_LANG_GROUP = 3
+
 class dynamodb_table:
     def __init__(self, table_name):
-        self._table_tweets_name = table_name
+        self._table_tweets_name = 'tweets'
         self._table_tweets = boto3.resource('dynamodb').Table(self._table_tweets_name)
-        # check if Tweets table exists
+        # check if tweets table exists
         try:
             self._table_tweets.creation_date_time
         except Exception:
-            self.create_tweet_table()
+            self.create_tweets_table()
+
+        self._table_users_name = 'users'
+        self._table_users = boto3.resource('dynamodb').Table(self._table_users_name)
+        # check if users table exists
+        try:
+            self._table_users.creation_date_time
+        except Exception:
+            self.create_users_table()
+
+        self._table_statistics_name = 'statistics'
+        self._table_statistics = boto3.resource('dynamodb').Table(self._table_statistics_name)
+        # check if statistics table exists
+        try:
+            self._table_statistics.creation_date_time
+        except Exception:
+            self.create_statistics_table()
+        self.clean_statistics_table()
 
     def write_tweets(self, tweets : list):
-        self.clean_tweet_table()
+        self.clean_tweets_table()
 
         with self._table_tweets.batch_writer() as batch:
             for tweet in tweets:
                 batch.put_item(Item=tweet)
+
+    def write_users(self, users : dict):
+        self.clean_users_table()
+
+        with self._table_users.batch_writer() as batch:
+            for _,user in users.items():
+                batch.put_item(Item=user)
+
+    def write_statistic(self, type, statistic) :
+        statistic = {
+            'type' : type,
+            'data' : statistic
+        }
+        # write statistic
+        self._table_statistics.put_item(Item=statistic)
     
-    def create_tweet_table(self):
-        tweet_table_schema['TableName'] = self._table_tweets_name
+    def create_tweets_table(self):
         dynamodb = boto3.resource('dynamodb')
 
-        self._table_tweets = dynamodb.create_table(**tweet_table_schema)
+        self._table_tweets = dynamodb.create_table(**tweets_table_schema)
         self._table_tweets.meta.client.get_waiter('table_exists').wait(TableName=self._table_tweets_name)
 
-    def clean_tweet_table(self):
+    def create_users_table(self):
+        dynamodb = boto3.resource('dynamodb')
+
+        self._table_users = dynamodb.create_table(**users_table_schema)
+        self._table_users.meta.client.get_waiter('table_exists').wait(TableName=self._table_users_name)
+
+    def create_statistics_table(self):
+        dynamodb = boto3.resource('dynamodb')
+
+        self._table_statistics = dynamodb.create_table(**statistics_table_schema)
+        self._table_statistics.meta.client.get_waiter('table_exists').wait(TableName=self._table_statistics_name)
+
+    def clean_tweets_table(self):
         # list all tweet
         scan = self._table_tweets.scan(FilterExpression=Attr('id').gt(0))
         # delete each tweet
@@ -61,6 +149,25 @@ class dynamodb_table:
             for each in scan['Items']:
                 batch.delete_item(Key={
                     'id':each['id'],
-                    'category':each['category']
+                    'hashtag':each['hashtag']
                 })
 
+    def clean_users_table(self):
+        # list all users
+        scan = self._table_users.scan(FilterExpression=Attr('id').gt(0))
+        # delete each users
+        with self._table_users.batch_writer() as batch:
+            for each in scan['Items']:
+                batch.delete_item(Key={
+                    'id':each['id']
+                })
+
+    def clean_statistics_table(self):
+        # list all users
+        scan = self._table_statistics.scan(FilterExpression=Attr('type').gt(0))
+        # delete each users
+        with self._table_statistics.batch_writer() as batch:
+            for each in scan['Items']:
+                batch.delete_item(Key={
+                    'type':each['type']
+                })
--- twitter_consumer.py
+++ twitter_consumer.py
@@ -1,34 +1,118 @@
 from twitter_interface import twitter_interface
-from dynamodb import dynamodb_table
+from dynamodb import dynamodb_table, statistics_type
+from datetime import datetime
 import json
 import time
+
 
 def twitter_datetime(datetime):
     return int(time.mktime(time.strptime(datetime,'%a %b %d %H:%M:%S +0000 %Y')) // 3600 * 3600)
 
-if __name__ == "__main__":
-    tweets_table = dynamodb_table('tweets')
-    with open("twitter_keys.json", 'r') as twt_keys:
-        keys = json.load(twt_keys)
-        interface = twitter_interface(**keys)
+def query_tags(twitter_api : twitter_interface):
+    tweets = []
+    users = {}
 
-        with open("tags.json", 'r') as twt_tags:
-            tags = json.load(twt_tags)['tags']
-            tweets = []
-            for tag in tags:                
-                result = interface.search_hashtag(tag)
-                for tweet in result['statuses']:
-                    tweet_entry = {}
-                    tweet_entry['id'] = tweet['id']
-                    tweet_entry['category'] = tag
-                    tweet_entry['text'] = tweet['text']  
-                    tweet_entry['date_time'] = twitter_datetime(tweet['created_at'])
-                    tweet_entry['user_id'] = tweet['user']['id']
-                    tweet_entry['user_name'] = tweet['user']['screen_name']
-                    tweet_entry['user_name'] = tweet['user']['screen_name']
-                    tweet_entry['user_followers'] = tweet['user']['followers_count']
-                    tweet_entry['user_lang'] = tweet['user']['lang']
-                    tweets.append(tweet_entry)
-            # save tweets on data base
-            tweets_table.write_tweets(tweets)
+    with open("tags.json", 'r') as twt_tags:
+        tags = json.load(twt_tags)['tags']
+        
+        for tag in tags:                
+            result = twitter_api.search_hashtag(tag)
+            for tweet in result['statuses']:
+                tweet_entry = {}
+                tweet_entry['id'] = tweet['id']
+                tweet_entry['hashtag'] = tag
+                tweet_entry['text'] = tweet['text']  
+                tweet_entry['date_time'] = tweet['created_at']
+                # user info
+                user_id = tweet['user']['id']
+                users[user_id] = {}
+                users[user_id]['id'] = tweet['user']['id']
+                users[user_id]['name'] = tweet['user']['screen_name']
+                users[user_id]['followers'] = tweet['user']['followers_count']
+                users[user_id]['lang'] = tweet['user']['lang']
+                tweet_entry['user'] = users[user_id]
+                # save tweet
+                tweets.append(tweet_entry)
+    
+    return tweets, users
+
+# find the five users with most followers
+def five_most_followed(users : dict):
+    statistic = []
+
+    sorted_users = sorted(users.values(), key=lambda item : item['followers'], reverse=True)
+    count = 0
+
+    for user in sorted_users:
+        if count >= 5:
+            break
+        else:
+            count += 1
+        statistic.append({
+            'user_id': user['id'],
+            'user_followers':user['followers']
+        })
+
+    return statistic
+
+# group and count tweets by hour of day
+def tweets_hour_of_day(tweets : list):
+    statistic = []
+
+    date_time_group = {}
+    for tweet in tweets:
+        timestamp = twitter_datetime(tweet['date_time'])
+        date_time_group.setdefault(timestamp, 0) 
+        date_time_group[timestamp] += 1
+
+    for key, value in date_time_group.items():
+        statistic.append({
+            'timestamp' : key,
+            'count' : value
+        })
+
+    return statistic
+
+# group and count tweets by lang and hashtag
+def tweets_lang_tag(tweets : list):
+    statistic = []
+
+    hashtag_group = {}
+    for tweet in tweets:
+        hashtag = tweet['hashtag']
+        lang = tweet['user']['lang']
+        hashtag_group.setdefault(hashtag, {})
+        hashtag_group[hashtag].setdefault(lang, 0)
+        hashtag_group[hashtag][lang] += 1
+
+    for hashtag, lang_group in hashtag_group.items():
+        lang_count = []
+        for lang, count in lang_group.items():
+            lang_count.append({
+                'lang': lang,
+                'count': count
+            })
+        statistic.append({
+            'hashtag': hashtag,
+            'lang_group': lang_count
+        })
+
+    return statistic
+
+if __name__ == "__main__":
+    data_base = dynamodb_table('tweets')
+    with open("twitter_keys.json", 'r') as twt_keys:
+        keys = json.load(twt_keys)\
+        # load twitter api
+        twitter_api = twitter_interface(**keys)
+        # query tweet from twitter api
+        tweets, users = query_tags(twitter_api)       
+        # save tweets on data base
+        data_base.write_tweets(tweets)
+        # save users on data base
+        data_base.write_users(users)
+        # generate statistics and save
+        data_base.write_statistic(statistics_type.FIVE_MOST, five_most_followed(users))
+        data_base.write_statistic(statistics_type.HOUR_DAY_GROUP, tweets_hour_of_day(tweets))
+        data_base.write_statistic(statistics_type.HASHTAG_LANG_GROUP, tweets_lang_tag(tweets))
 
